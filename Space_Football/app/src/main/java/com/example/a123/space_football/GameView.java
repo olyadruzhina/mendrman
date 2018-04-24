440147ebfd87e08d0564e82b20f13b535a0b2b20
olyadruzhina <38502907+olyadruzhina@users.noreply.github.com>
2018-04-24 10:45:47 +0300

    Update GameView.java

--- Space_Football/app/src/main/java/com/example/a123/space_football/GameView.java
+++ Space_Football/app/src/main/java/com/example/a123/space_football/GameView.java
@@ -1,4 +1,3 @@
-
 package com.example.a123.space_football;
 
 import android.content.Context;
@@ -8,13 +7,12 @@ import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.widget.TextView;
-
 import java.util.Random;
+
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
@@ -23,17 +21,12 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     Random rand = new Random();
     int[] x = new int[100];
     int[] y = new int[100];
-    private CharacterSprite player1, player2, asteroid, star1, star2, star3, star4;
-    boolean moving = false;
-    Rect hero1, hero2;
-    int goal1 = 0, goal2 = 0;
-    boolean update;
-    private int mActivePointerId;
-    private long lastDrawNanoTime = -1;
-
-
-    int upPI = 0;
-    int downPI = 0;
+    private CharacterSprite player1, player2, asteroid;
+    int goal1 = 0, goal2 = 0, pointerId1= -1, pointerId2= -1, playerHeight, playerWidth, asteroidHeight, asteroidWidth;
+    private final int edge = 10;
+    private  boolean b_goal1, b_goal2;
+    private int xVelocity = 5;
+    private int yVelocity = 5;
 
     public GameView(Context context) {
         super(context);
@@ -62,80 +55,89 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         player2.y = screenHeight - player2.image.getHeight() - 150;
         asteroid.x = screenWidth / 2 - asteroid.image.getWidth() / 2;
         asteroid.y = screenHeight / 2 - asteroid.image.getHeight() / 2;
+        playerHeight = player1.image.getHeight();
+        playerWidth = player1.image.getWidth();
+        asteroidWidth = asteroid.image.getWidth();
+        asteroidHeight = asteroid.image.getHeight();
 
         for (int i = 0; i < 100; i++) {
             x[i] = rand.nextInt(screenWidth);
             y[i] = rand.nextInt(screenHeight);
         }
 
-
         thread.setRunning(true);
         thread.start();
-
     }
 
 
     //@Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        for (int i = 0; i < event.getPointerCount(); i++) {
-            //   mActivePointerId = event.getPointerId(0);
-            //    int action = event.getActionMasked();
-            int pointerIndex = event.getActionIndex();
+        int count = event.getPointerCount();
+        int maskedAction = event.getActionMasked();
 
-            // get pointer ID
-            int pointerId = event.getPointerId(pointerIndex);
-
-            // get masked (not specific to a pointer) action
-            int maskedAction = event.getActionMasked();
-
-            //int pointerIndex = event.findPointerIndex(mActivePointerId);
-            float x = event.getX(pointerIndex);
-            float y = event.getY(pointerIndex);
-
+        for (int i = 0; i < count; i++) {
             switch (maskedAction) {
 
                 case MotionEvent.ACTION_DOWN: {
-                    break;
                 }
                 case MotionEvent.ACTION_POINTER_DOWN: {
-                    // TODO use data
+
+                    if (pointerIsOnPlayer(i, event, player1) && pointerId1 == -1) {
+                        pointerId1 = event.getPointerId(event.getActionIndex());
+                    }
+                    if (pointerIsOnPlayer(i, event, player2) && pointerId2 == -1) {
+                        pointerId2 = event.getPointerId(event.getActionIndex());
+                    }
+                }
+
+                case MotionEvent.ACTION_MOVE: {
+                    if (i == pointerId1) {
+                        player1.x = (int) event.getX(event.findPointerIndex(pointerId1)) - playerWidth / 2;
+                        player1.y = (int) event.getY(event.findPointerIndex(pointerId1)) - playerHeight / 2;
+
+                        if (player1.y > screenHeight / 2 - playerHeight)
+                            player1.y = screenHeight / 2 - playerHeight - edge;
+                        if (player1.y < edge) player1.y = edge;
+                        if (player1.x < edge) player1.x = edge;
+                        if (player1.x + playerWidth > screenWidth - edge)
+                            player1.x = screenWidth - playerWidth - edge;
+                    }
+                    if (i == pointerId2) {
+                        player2.x = (int) event.getX(event.findPointerIndex(pointerId2)) - playerWidth / 2;
+                        player2.y = (int) event.getY(event.findPointerIndex(pointerId2)) - playerHeight / 2;
+
+                        if (player2.y <= screenHeight / 2) player2.y = screenHeight / 2;
+                        if (player2.y + playerHeight > screenHeight - edge)
+                            player2.y = screenHeight - playerHeight - edge;
+                        if (player2.x < edge) player2.x = edge;
+                        if (player2.x + playerWidth > screenWidth - edge)
+                            player2.x = screenWidth - playerWidth - edge;
+                    }
                     break;
                 }
-                case MotionEvent.ACTION_MOVE: { // a pointer was moved
-                    // TODO use data
-                    break;
-                }
+
                 case MotionEvent.ACTION_UP: {
-                    break;
                 }
                 case MotionEvent.ACTION_POINTER_UP: {
+
+                    if (event.getPointerId(event.getActionIndex()) == pointerId1) {
+                        pointerId1 = -1;
+                    }
+                    if (event.getPointerId(event.getActionIndex()) == pointerId2) {
+                        pointerId2 = -1;
+                    }
                     break;
                 }
-            }
-
-
-            if (pointerId == 0) {
-                player2.x = (int) x - player2.image.getWidth() / 2;
-                player2.y = (int) y - player2.image.getHeight() / 2;
-                if (player2.y <= screenHeight / 2) player2.y = screenHeight / 2;
-                if (player2.y + player2.image.getHeight() > screenHeight)
-                    player2.y = screenHeight - player2.image.getHeight();
-                if (player2.x < 0) player2.x = 0;
-                if (player2.x + player2.image.getWidth() > screenWidth)
-                    player2.x = screenWidth - player2.image.getWidth();
-            } else {
-               player1.x = (int) x - player1.image.getWidth() / 2;
-                player1.y = (int) y - player1.image.getHeight() / 2;
-                    if (player1.y > screenHeight / 2)
-                   player1.y = screenHeight / 2 - player2.image.getHeight();
-                if (player1.y < 0) player1.y = 0;
-                if (player1.x < 0) player1.x = 0;
-                if (player1.x + player1.image.getWidth() > screenWidth)
-                    player1.x = screenWidth - player1.image.getWidth();
             }
         }
         return true;
+    }
+
+    public boolean pointerIsOnPlayer(int i, MotionEvent event, CharacterSprite player){
+        if (event.getY(i) < player.y + playerHeight && event.getY(i) > player.y && event.getX(i) < player.x + playerWidth && event.getX(i) > player.x)
+            return  true;
+        else return  false;
     }
 
     @Override
@@ -179,16 +181,6 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
             canvas.drawLine(5, 5, 5, screenHeight - 5, paint);
             canvas.drawLine(screenWidth - 5, 5, screenWidth - 5, screenHeight - 5, paint);
 
-
-            if (asteroid.goal1) {
-                goal2++;
-                asteroid.goal1 = false;
-            }
-            if (asteroid.goal2) {
-                goal1++;
-                asteroid.goal2 = false;
-            }
-
             paint.setTextSize(50);
             paint.setMaskFilter(new BlurMaskFilter(10, BlurMaskFilter.Blur.SOLID));
             canvas.drawText("GOALS", screenWidth - 200, 70, paint);
@@ -202,16 +194,72 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
+public boolean isCollision(CharacterSprite asteroid, CharacterSprite hero) {
+
+        if (hero.x < asteroid.x + asteroid.image.getWidth() &&
+            hero.x + hero.image.getWidth() > asteroid.x &&
+            hero.y < asteroid.y + asteroid.image.getHeight() &&
+            hero.y + hero.image.getHeight() > asteroid.y) {
+        xVelocity = xVelocity * -1;
+        yVelocity = yVelocity * -1;
+        return true;
+    } else return false;
+}
 
     public void update() {
 
-        hero1 = new Rect(player1.x / 1, player1.y / 1, player1.x + player1.image.getWidth(), player1.y + player1.image.getHeight());
-        hero2 = new Rect(player2.x / 1, player2.y / 1, player2.x + player2.image.getWidth(), player2.y + player1.image.getHeight());
-        asteroid.update(hero1);
-        asteroid.update(hero2);
 
+        asteroid.x = asteroid.x + xVelocity/10;
+        asteroid.y = asteroid.y + yVelocity/10;
 
+        if (isCollision(asteroid, player1) ) {
 
+            xVelocity = asteroid.x - player1.x ;
+            yVelocity = asteroid.y - player1.y ;
+        }
 
+        if (isCollision(asteroid, player2)){
+            xVelocity = asteroid.x - player2.x ;
+            yVelocity = asteroid.y - player2.y ;
+        }
+
+        if (asteroid.x > screenWidth - asteroidWidth - edge || asteroid.x < edge) {
+            xVelocity = xVelocity * -1;
+        }
+
+        if ((asteroid.y > screenHeight - asteroidHeight - edge || asteroid.y < edge) && (asteroid.x  < screenWidth / 2 - 150 || asteroid.x + asteroidWidth > screenWidth / 2 + 150)) {
+            yVelocity = yVelocity * -1;
+        }
+
+        if (asteroid.y > screenHeight - asteroidHeight){
+            b_goal2 = true;
+        }
+
+        if (asteroid.y < 0) {
+            b_goal1 = true;
+        }
+
+        if (b_goal1 || b_goal2) {
+            player1.x = screenWidth / 2 - player1.image.getWidth() / 2;
+            player1.y = 150;
+            player2.x = screenWidth / 2 - player2.image.getWidth() / 2;
+            player2.y = screenHeight - player2.image.getHeight() - 150;
+            asteroid.x = screenWidth / 2 - asteroidWidth / 2;
+            asteroid.y = screenHeight / 2 - asteroidHeight / 2;
+            xVelocity = 0;
+            yVelocity = 0;
+            pointerId1 = -1;
+            pointerId2 = -1;
+        }
+
+        if (b_goal1) {
+            goal2++;
+            b_goal1 = false;
+        }
+
+        if (b_goal2) {
+            goal1++;
+            b_goal2 = false;
+        }
     }
 }
